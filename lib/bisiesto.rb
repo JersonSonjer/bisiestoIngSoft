44c403d6abddfe797592d1f86f840c000272381c
jerson <jersongarciavacaflor@gmail.com>
2021-04-26 23:13:20 -0400

    Mi implementacion3 cualquier anio que es divisible entre 4 pero no entre 100 a no ser que sea divisible entre 400 pasando

--- lib/bisiesto.rb
+++ lib/bisiesto.rb
@@ -1,5 +1,5 @@
 def es_bisiesto(anio)
-    if(anio%4==0 && anio%100!=0)
+    if((anio%4==0 && anio%100!=0)|| anio %400==0)
         return true
     end
     return false
--- spec/es_bisiesto_spec.rb
+++ spec/es_bisiesto_spec.rb
@@ -21,5 +21,12 @@ RSpec.describe "bisiesto" do
     it "deberia devolver false para cualquier anio que es divisible entre 4 y tambien entre 100"do
         expect(es_bisiesto(1400)).to eq(false)
     end
+
+    it "deberia devolver true para cualquier anio que es divisible entre 4 pero NO entre 100 a no ser que sea divisible entre 400"do
+        expect(es_bisiesto(1600)).to eq(true)
+    end
+    it "deberia devolver false para cualquier anio que es divisible entre 4 y tambien divisible entre 100 pero que NO sea divisible entre 400"do
+    expect(es_bisiesto(1800)).to eq(false)
+end
 end
 
